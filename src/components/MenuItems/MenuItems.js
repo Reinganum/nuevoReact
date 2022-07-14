ec6824b9aac4a84274971eaea078ceef6443053d
Harijan Fernandez <har.fernandez@hotmail.com>
2022-07-14 17:29:15 -0400

    primer commit totalmente generico

---
+++ src/components/MenuItems/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems=[
+    {
+        label:'Inicio',
+        url:'#',
+        cName:'nav-links'
+    },
+    {
+        label:'Nosotros',
+        url:'#',
+        cName:'nav-links'
+    },
+    {
+        label:'Productos',
+        url:'#',
+        cName:'nav-links'
+    },
+    {
+        label:'Blog',
+        url:'#',
+        cName:'nav-links'
+    },
+    {
+        label:'Contacto',
+        url:'#',
+        cName:'nav-links'
+    },
+]
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {MenuItems} from './MenuItems'
+
+class NavBar extends Component {
+    render(){
+        return(
+            <nav className='NavBarItems'>
+                <h1 className='NavBar-logo'>Proyecto</h1>
+                <div className='Menu-icon'>
+                </div>
+                <ul>
+                    <li></li>
+                </ul>
+            </nav>
+        )
+    }
+};
+
+export default Navbar
